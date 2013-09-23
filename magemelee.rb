7a547316441f6e80e4f12d27721783ce5ffe011d
Colin <vogon@icculus.org>
2013-09-22 18:22:47 -0700

    session test

--- magemelee.rb
+++ magemelee.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
 
 class MageMeleeApp < Sinatra::Base
+	enable :sessions
+
 	get '/' do
 		"hello world!"
+	end
+
+	get '/login' do
+		session[:logged_in] = "yup"
+		"done"
+	end
+
+	get '/is_logged_in' do
+		"logged_in: #{session[:logged_in]}"
 	end
 end
