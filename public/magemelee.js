5f8d151a14f6c69455a37d08f23972337dc2c611
Colin <vogon@outlook.com>
2013-10-01 15:00:10 -0700

    replace mustache.js with pure.js, which has garbage documentation but apparently seems to work

--- public/magemelee.js
+++ public/magemelee.js
@@ -17,11 +17,15 @@ function newGame() {
 	$.post(ajaxURI).done(updateGameList);
 }
 
+var fn;
+
 function updateGameList() {
 	$.getJSON("/ajax/games")
 		.done(function(games) {
-			var renderedTemplate = makeGameList(games);
-			$(".game-list-container").html(renderedTemplate);
+			// rerender template using new game list
+			fn = fn || $(".game-list").compile(null, games);
+			$(".game-list").render(games, fn);
+
 			$("#create-game").click(newGame);
 		});
 }
